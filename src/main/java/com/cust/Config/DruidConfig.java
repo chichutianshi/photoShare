b933bde2cf5c58c794f0c6e36766e14bfecbfb93
cyka <1354250064mdzz@gmail.com>
2019-04-24 19:36:07 +0800

    druid监控

--- src/main/java/com/cust/Config/DruidConfig.java
+++ src/main/java/com/cust/Config/DruidConfig.java
@@ -1,7 +1,11 @@
 package com.cust.Config;
 
 import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.support.http.StatViewServlet;
+import com.alibaba.druid.support.http.WebStatFilter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.cache.CacheManager;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +13,7 @@ import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import javax.sql.DataSource;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -20,13 +25,40 @@ public class DruidConfig {
         return new DruidDataSource();
     }
 
+    //配置druid监控
+    //1、配置管理后台servlet
+    @Bean
+    public ServletRegistrationBean statViewServlet() {
+        ServletRegistrationBean bean = new ServletRegistrationBean(new StatViewServlet(), "/druid/*");
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("loginUsername", "admin");
+        initParams.put("loginPassword", "123456");
+        initParams.put("allow", ""); //默认允许所有
+        initParams.put("deny", "192.168.1.119");
+        bean.setInitParameters(initParams);
+        return bean;
+    }
+
+
+    //2、配置监控filter
+
+    @Bean
+    public FilterRegistrationBean webStatFilter() {
+        FilterRegistrationBean bean = new FilterRegistrationBean();
+        bean.setFilter(new WebStatFilter());
+        Map<String, String> initParams = new HashMap<>();
+        initParams.put("exclusions", "*.js,*.css,/druid/*");
+        bean.setInitParameters(initParams);
+        bean.setUrlPatterns(Arrays.asList("/*"));
+        return bean;
+    }
 
     @Bean
     public CacheManager cacheManager(RedisTemplate redisTemplate) {
-        RedisCacheManager cacheManager= new RedisCacheManager(redisTemplate);
+        RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
         cacheManager.setDefaultExpiration(60);
-        Map<String,Long> expiresMap=new HashMap<>();
-        expiresMap.put("Product",5L);
+        Map<String, Long> expiresMap = new HashMap<>();
+        expiresMap.put("Product", 5L);
         cacheManager.setExpires(expiresMap);
         return cacheManager;
     }
